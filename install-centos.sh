a04a35a9e6716882361b333eaedf4d559520e50b
Jason Jack <jasonajack@gmail.com>
2017-06-26 00:39:36 -0400

    Added command to include fetching from upstream repository

--- install-centos.sh
+++ install-centos.sh
@@ -5,6 +5,10 @@ sudo yum install -y php php-mbstring wkhtmltopdf composer vim firefox
 
 # Pull submodules
 git submodule update --init
+pushd markdown-resume
+git remote add upstream git@github.com:there4/markdown-resume.git
+git fetch upstream
+popd
 
 # Build submodule
 pushd markdown-resume
